2534b64d02a3c9a00cadfc37fdf339a8effacc9f
Erick Santos <erick.ems@outlook.com>
2023-05-30 11:56:21 -0300

    #09-arrumando parametros da tabela
    Co-authored-by: Bianca Sofia <sofiabrasil44@gmail.com>

--- src/domain/entities/Curso.py
+++ src/domain/entities/Curso.py
@@ -9,10 +9,10 @@ class Curso(Base):
     '''Classe para estabelecer o modelo na tabela DB'''
     __tablename__ = "cursos"
 
-    id = int = Column(Integer, primary_key = True, index = True)
-    nome = str = Column(String(70), nullable = False)
-    descricao = Column(String(300), nullable = False)
-    duracaoHoras = Column(Integer, nullable = False)
+    id: int = Column(Integer, primary_key = True, index = True)
+    nome : str = Column(String(70), nullable = False)
+    descricao : Column(String(300), nullable = False)
+    duracaoHoras : Column(Integer, nullable = False)
 
 class CursoBase(BaseModel):
     nome: str
