12db7406cf0e8b589e20552fb21e574f3fe8c2fa
caioLelis <cayolelisdf@gmail.com>
2023-05-31 22:07:07 -0300

    Atualizando entities Co-authored-by: Doan Filho <doangalvaofilho@gmail.com>

--- src/domain/entities/Turma.py
+++ src/domain/entities/Turma.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from entities.Curso import Curso
 from enum import Enum
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Index
@@ -9,11 +10,14 @@ Base = declarative_base()
 
 class ClassRoomBase(BaseModel):
     codigo:int 
-    dataInicio: str
+    nome_turma: str
+    data_inicio: str
     data_fim: str
-    horario_inicio:str 
-    horario_inicio:str 
-    capacidade_turma:int 
+    inicio_aula:str 
+    fim_aula:str 
+    capacidade_turma:int
+    fk_curso:int
+    professor:str 
 
 # Pre requisito -> Crud prof 
 ##Classe turma sem atributos de curso e professor 
@@ -21,13 +25,16 @@ class ClassRoomDB(Base):
     __tablename__= "class"
     __table_args__ = {"extend_existing": True}
 
-    codigo:int = Column(Integer(70), primary_key= True , nullable= False)
-    dataInicio: str= Column(String(10), nullable= False)
-    data_fim: str= Column(String(10), nullable= False)
-    horario_inicio:str = Column(String(10), nullable=False)
-    horario_inicio:str = Column(String(10), nullable=False)
-    capacidade_turma:int = Column(String(10), nullable=False)
 
+    codigo:int = Column(Integer(), primary_key= True , nullable= False)
+    nome_turma: str = Column(String(70), nullable= False)
+    data_inicio: str= Column(String(10), nullable= False)
+    data_fim: str= Column(String(10), nullable= False)
+    inicio_aula:str = Column(String(10), nullable=False)
+    fim_aula:str = Column(String(10), nullable=False)
+    capacidade_turma:int = Column(String(10), nullable=False)
+    fk_curso:int = Column(Integer, ForeignKey(Curso.id))
+    fk_professor:str = Column(String(), nullable=False)
 
 class ClassRoomRequest(ClassRoomDB):
    '''...'''
