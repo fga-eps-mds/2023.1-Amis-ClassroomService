bdcfd106c0e9ac84e7529b1753d74091bae4ad03
biancasofia <sofiabrasil44@gmail.com>
2023-06-11 18:26:06 -0300

    #16-add model e schema de tabela
    Co-Authored-By: iagorrr <85769349+iagorrr@users.noreply.github.com>
    Co-Authored-By: Erick Santos <79948051+Erick-ems@users.noreply.github.com>

---
+++ src/domain/entities/InstrucaoCapacitacao.py
@@ -0,0 +1,38 @@
+'''Importando parâmetros da orm'''
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from database import Base
+from pydantic import BaseModel
+
+class InstrucaoCapacitacao(Base):
+    '''Classe para estabelecer o modelo na tabela DB'''
+    __tablename__ = "instrucaoCapacitacao"
+    __table_args__ = {"extend_existing": True}
+
+    idCurso: int = Column("idCurso", ForeignKey("curso.id"), index=True)
+    id: int = Column(Integer, primary_key = True, index = True)
+    nome : str = Column(String(70), nullable = False)
+    descricao : str = Column(String(800), nullable = False)
+    dataCadastro : str= Column(Date, nullable = False)
+
+
+class InstrucaoCapacitacaoBase(BaseModel):
+    idCurso: int
+    nome: str
+    descricao: str
+    dataCadastro: str
+    
+
+class InstrucaoCapacitacaoRequest(InstrucaoCapacitacaoBase):
+    '''...'''
+    pass
+
+class InstrucaoCapacitacaoRequestId(InstrucaoCapacitacaoBase):
+    """Necessário para se fazer o update"""
+    id : int
+
+class InstrucaoCapacitacaoResponse(InstrucaoCapacitacaoBase):
+    '''...'''
+    id:int
+    class Config:
+        orm_mode = True
+
