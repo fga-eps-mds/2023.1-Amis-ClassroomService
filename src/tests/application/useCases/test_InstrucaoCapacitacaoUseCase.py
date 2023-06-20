72e9921860a893fd7da450bc2778ec77d064cde2
biancasofia <sofiabrasil44@gmail.com>
2023-06-20 18:49:00 -0300

    #16-add update test
    Co-Authored-By: Erick Santos <79948051+Erick-ems@users.noreply.github.com>
    Co-Authored-By: iagorrr <85769349+iagorrr@users.noreply.github.com>

--- src/tests/application/useCases/test_InstrucaoCapacitacaoUseCase.py
+++ src/tests/application/useCases/test_InstrucaoCapacitacaoUseCase.py
@@ -2,7 +2,7 @@
 from application.useCases.CadastrarInstrucaoCapacitacaoUseCase import InstrucaoCapacitacaoUseCase
 from infrastructure.repositories.InstrucaoCapacitacaoRepository import InstrucaoCapacitacaoRepository
 
-from domain.entities.InstrucaoCapacitacao import InstrucaoCapacitacao
+from domain.entities.InstrucaoCapacitacao import InstrucaoCapacitacao, InstrucaoCapacitacaoBase
 from sqlalchemy.orm import Session
 from unittest.mock import MagicMock
 import pytest
@@ -25,15 +25,15 @@ instrucao_use_case = InstrucaoCapacitacaoUseCase(
 test_list = [
     (InstrucaoCapacitacao(
         id=500,
-        nome="Curso de Patchwork",
+        nome="Instruções de patchwork",
         descricao="Aprenda a técnica do patchwork e crie belos trabalhos em tecido.",
         dataCadastro="2022-01-01",
         idCurso= 36,
     )),
     (InstrucaoCapacitacao(
         id=501,
-        nome="Curso de Bolo",
-        descricao="Aprenda a fazer olo.",
+        nome="Receita de Bolo",
+        descricao="2 xícaras de massa e açúcar",
         dataCadastro="2022-01-01",
         idCurso= 36,
     )),
@@ -44,10 +44,12 @@ test_list = [
     "instrucaoCapacitacaoSent",
     test_list,
 )
+
+
 def test_save_instrucao_valida(instrucaoCapacitacaoSent):
     """Testa se o curso é salvo com sucesso, assume que sempre recebe um curso válido"""
-    print(instrucaoCapacitacaoSent.__dict__)
-    print(instrucao_use_case.find_all())
+    #print(instrucaoCapacitacaoSent.__dict__)
+    #print(instrucao_use_case.find_all())
     response = instrucao_use_case.save(instrucaoCapacitacaoSent=instrucaoCapacitacaoSent)
     assert instrucaoCapacitacaoSent == response, response.__dict__
 
@@ -63,3 +65,28 @@ def test_instrucao_find_all_ja_cadastrados():
     print(expected)
 
     assert len(response) == len(test_list), response
+
+
+
+def test_update_instrucao_valida():
+    instrucao_existente = InstrucaoCapacitacao(
+        id=500,
+        nome="Curso de Patchwork",
+        descricao="Aprenda a técnica do patchwork e crie belos trabalhos em tecido.",
+        dataCadastro="2022-01-01",
+        idCurso=36,
+    )
+    instrucao_use_case.save(instrucaoCapacitacaoSent=instrucao_existente)
+
+    instrucao_atualizada = InstrucaoCapacitacao(
+        id=500,
+        nome="Curso de Patchwork",
+        descricao="Aprenda a técnica do patchwork.",
+        dataCadastro="2022-01-01",
+        idCurso=36,
+    )
+
+    response = instrucao_use_case.update(instrucaoCapacitacaoSent=instrucao_atualizada)
+    # Verificação se a resposta da instrução de capacitação foi atualizada corretamente
+    assert instrucao_atualizada == response
+
