1c449f651dd4655960eb54d749a1ceb7d3b1a474
biancasofia <sofiabrasil44@gmail.com>
2023-06-20 16:42:13 -0300

    #16-update

--- src/application/controllers/InstrucaoCapacitacaoController.py
+++ src/application/controllers/InstrucaoCapacitacaoController.py
@@ -51,10 +51,22 @@ def read_by_id(idCurso: int) -> list[InstrucaoCapacitacaoResponse]:
 # UPDATE
 @router_instrucao.put("/{id}", status_code=status.HTTP_201_CREATED)
 def update(sent: InstrucaoCapacitacaoRequest):
+    to_save = InstrucaoCapacitacao(**sent.__dict__)
     if instrucaoCapacitacaoUseCase.find_by_id(sent.id) is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND,
                             detail="Instrução não existente")
-    instrucaoCapacitacaoUseCase.update(sent)
+    response = instrucaoCapacitacaoUseCase.update(
+        instrucaoCapacitacaoSent=to_save)
+    if response is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Não foi possível atualizar a instrução de capacitação",
+        )
+    
+
+ 
+    
+
 
 # DETELE (by id)
 @router_instrucao.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
